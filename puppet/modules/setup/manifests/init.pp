c514468a1a9dc3ba1dcb6941968a026dc3b06beb
Linda Nichols <lynnaloo@gmail.com>
2015-10-05 10:07:45 -0400

    Update Node version

--- puppet/modules/setup/manifests/init.pp
+++ puppet/modules/setup/manifests/init.pp
@@ -1,4 +1,4 @@
-class setup($node_version = "v0.12.0") {
+class setup($node_version = "v0.12.7") {
   # Add some default path values
   Exec { path => ['/usr/local/bin','/usr/local/sbin','/usr/bin/','/usr/sbin','/bin','/sbin', "/home/vagrant/nvm/${node_version}/bin"], }
 
